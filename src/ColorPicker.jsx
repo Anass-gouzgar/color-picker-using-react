b172ef46625c59ef11324dcc26c8792d11349cc8
anass-gouzgar <go.anass.gouzgar@gmail.com>
2024-01-25 23:08:25 +0100

    added the css code

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -11,8 +11,8 @@ function handleColorChange(e){
   return (
     <div className='color-picker-container'>
 <h1>Color Picker</h1>
-<div className="color-dispay" style={{backgroundColor: color}}>
-    <p>Selected Color : </p>
+<div className="color-display" style={{backgroundColor: color}}>
+<p>Selected Color : {color} </p>
 </div>
 <label> Select a Color:</label>
 <input type="color" value={color} onChange={handleColorChange} />
